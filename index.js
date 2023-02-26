646451d13be89d83142afd58b501013b2b1ed919
Malitowska <kristina.malitowska@gmail.com>
2023-02-26 22:50:55 +0200

    add Lesson

---
+++ index.js
@@ -0,0 +1,18 @@
+// import { name } from "./module";
+// import "./style.css";
+// import isEmail from "validator/lib/isEmail";
+// import isURL from "validator/lib/isURL";
+// import toDate from "validator/lib/toDate";
+// // console.log("Your name: " + name);
+
+// console.log(isEmail("foo@bar.com"));
+// console.log(
+//   isURL(
+//     "https://edo.vchasno.ua/app/documents?utm_source=document_info&utm_campaign=Prishli_documenty_na_podpis&utm_medium=email&cid=d83562d4-1a05-464c-94df-1ae14df19292&folder_id=6008"
+//   )
+// );
+// console.log(toDate("12.12.2000"));
+
+var moment = require("moment");
+
+console.log(moment().format("MMM Do YY"));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["./index"],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+  },
+};
